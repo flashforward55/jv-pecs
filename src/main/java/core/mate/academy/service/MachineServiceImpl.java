3adc946f809d356b6dbedd56624010501dd7e530
Anton Stiazhkin <rudana089@gmail.com>
2025-06-06 22:29:39 +0300

    These classes create 2–3 machines of the appropriate type and return a list. Now the entire system will work correctly with generics, according to the PECS principle.

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -1,7 +1,34 @@
 package core.mate.academy.service;
 
-/**
- * Your implementation of MachineService.
- */
-public class MachineServiceImpl {
+import core.mate.academy.model.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MachineServiceImpl<T extends Machine> implements MachineService<T> {
+
+    @Override
+    public List<? extends T> getAll(Class<? extends T> type) {
+        if (type == Bulldozer.class) {
+            return (List<? extends T>) new BulldozerProducer().get();
+        } else if (type == Truck.class) {
+            return (List<? extends T>) new TruckProducer().get();
+        } else if (type == Excavator.class) {
+            return (List<? extends T>) new ExcavatorProducer().get();
+        }
+        return new ArrayList<>();
+    }
+
+    @Override
+    public void fill(List<? super T> machines, T value) {
+        for (int i = 0; i < machines.size(); i++) {
+            machines.set(i, value);
+        }
+    }
+
+    @Override
+    public void startWorking(List<? extends T> machines) {
+        for (T machine : machines) {
+            machine.doWork();
+        }
+    }
 }

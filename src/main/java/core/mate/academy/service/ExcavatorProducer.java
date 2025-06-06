0f0ca7ecc75d3195f3ecbb3ac3366748d4328303
Anton Stiazhkin <rudana089@gmail.com>
2025-06-06 23:10:14 +0300

    These classes create 2–3 machines of the appropriate type and return a list. Now the entire system will work correctly with generics, according to the PECS principle.

--- src/main/java/core/mate/academy/service/ExcavatorProducer.java
+++ src/main/java/core/mate/academy/service/ExcavatorProducer.java
@@ -1,15 +1,12 @@
 package core.mate.academy.service;
 
 import core.mate.academy.model.Excavator;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ExcavatorProducer implements MachineProducer<Excavator> {
     @Override
     public List<Excavator> get() {
-        List<Excavator> excavators = new ArrayList<>();
-        excavators.add(new Excavator());
-        excavators.add(new Excavator());
-        return excavators;
+        return Arrays.asList(new Excavator(), new Excavator(), new Excavator());
     }
 }
